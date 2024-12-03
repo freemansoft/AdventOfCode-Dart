fa30441051fb6038431b001de2e3be91eb7208a6
Joe Freeman <joe@freemansoft.com>
2024-12-03 12:46:30 -0500

    rename

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -18,15 +18,15 @@ class Day03 extends GenericDay {
       final clauses = line.split('mul');
       print(clauses);
       for (var aClause in clauses) {
-        // the first right paren.  ignore th rest
-        var endParen = aClause.indexOf(')');
+        // the first right paren.  ignore the rest
+        final endParen = aClause.indexOf(')');
         if (endParen > 0 && aClause.startsWith('(')) {
-          var aClauseNoParen = aClause.substring(1, endParen);
+          final aClauseNoParen = aClause.substring(1, endParen);
           print('evaluate $aClause as ${aClauseNoParen}');
           try {
-            var operands = aClauseNoParen.split(',').map(int.parse).toList();
+            final operands = aClauseNoParen.split(',').map(int.parse).toList();
             if (operands.length == 2) {
-              var result = operands[0] * operands[1];
+              final result = operands[0] * operands[1];
               sumOfOperators += result;
             }
           } catch (e) {
