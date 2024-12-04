2cc345a23837743e80c4b324488357dde4b4a60a
Joe Freeman <joe@freemansoft.com>
2024-12-03 20:17:33 -0500

    day 3 part 2

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -11,6 +11,7 @@ class Day03 extends GenericDay {
 
   @override
   int solvePart1() {
+    print('*********** part 1 **************');
     var sumOfOperators = 0;
     final data = parseInput() as List<String>;
     for (var line in data) {
@@ -42,6 +43,56 @@ class Day03 extends GenericDay {
 
   @override
   int solvePart2() {
-    return 0;
+    print('*********** part 2 **************');
+    var sumOfOperators = 0;
+    final data = parseInput() as List<String>;
+    print('starting with $data');
+
+    // isActive spans lines
+    var isActive = true;
+    for (final line in data) {
+      // regex the heck out of this
+      final mulRegExp = RegExp(r"(don\'t\(\))|(do\(\))|mul\(\d+,\d+\)");
+
+      // all the matches
+      final match = mulRegExp.allMatches(line).toList();
+      //print('$match');
+
+      // split the matches into elements of a list
+      final macroTokens = match.map((e) => e[0]!).toList();
+      print('$macroTokens');
+
+      for (final token in macroTokens) {
+        if (token == "don't()") {
+          isActive = false;
+          print(token);
+        } else if (token == 'do()') {
+          isActive = true;
+          print(token);
+        } else if (token.startsWith('mul')) {
+          if (isActive) {
+            print('calculating $token');
+            final operands = token
+                .substring(4, token.length - 1)
+                .split(',')
+                .map(int.parse)
+                .toList();
+            if (operands.length == 2) {
+              final result = operands[0] * operands[1];
+              print('operands: $operands adding $result to $sumOfOperators');
+              sumOfOperators += result;
+              print('operands: $operands resulting in $sumOfOperators');
+            } else {
+              print('bad operands $operands staying at $sumOfOperators');
+            }
+          } else {
+            // shouldn't happen
+            print('ignoring $token');
+          }
+        }
+      }
+    }
+    // 92082041
+    return sumOfOperators;
   }
 }
