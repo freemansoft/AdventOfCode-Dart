f6eb8d3a91ab99561e7e8f375bc4a361cb62d4cf
Joe Freeman <joe@freemansoft.com>
2024-12-15 10:07:55 -0500

    Init day 15

---
+++ solutions/day15.dart
@@ -0,0 +1,23 @@
+import '../utils/index.dart';
+
+class Day15 extends GenericDay {
+  Day15() : super(15);
+
+  @override
+  parseInput() {
+
+  }
+
+  @override
+  int solvePart1() {
+
+    return 0;
+  }
+
+  @override
+  int solvePart2() {
+
+    return 0;
+  }
+}
+
