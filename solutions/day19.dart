a4c5b9a27e50cf04088c16d761cb007cd8577ea8
Joe Freeman <joe@freemansoft.com>
2024-12-22 10:52:06 -0500

    Day 19 part 1

--- solutions/day19.dart
+++ solutions/day19.dart
@@ -1,23 +1,53 @@
+import 'dart:math';
+
 import '../utils/index.dart';
 
 class Day19 extends GenericDay {
   Day19() : super(19);
 
-  @override
-  parseInput() {
+  List<String> availableTowels = <String>[];
 
+  @override
+  List<String> parseInput() {
+    final theInput = InputUtil(19).getParagraphLines().toList();
+    // we know the first pargraph is just the towel set
+    availableTowels = theInput[0][0].replaceAll(' ', '').split(',');
+    return theInput[1];
   }
 
   @override
   int solvePart1() {
+    final lookingFor = parseInput();
 
-    return 0;
+    final dogfood = lookingFor.map((e) {
+      final results = scanIt(e, availableTowels, e);
+      print(results);
+      return results;
+    }).toList();
+    return dogfood.fold(
+        0, (previousCount, solvedThis) => previousCount + (solvedThis ? 1 : 0));
   }
 
   @override
   int solvePart2() {
-
     return 0;
   }
-}
 
+  /// base string is the original request value and is passed in for debugging
+  bool scanIt(String e, List<String> availableTowels, String baseString) {
+    if (e.isEmpty) {
+      return true;
+    }
+    for (final towel in availableTowels) {
+      if (e.startsWith(towel) &&
+          scanIt(
+            e.substring(towel.length),
+            availableTowels,
+            baseString,
+          )) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
